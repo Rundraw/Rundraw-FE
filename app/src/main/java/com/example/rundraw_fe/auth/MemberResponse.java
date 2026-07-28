4c738b916ea8cedc0f3ba79ef9b6270842a03315
Anna0626 <lsr00626@naver.com>
2026-07-28 14:17:42 +0900

    feat: OAuth 신규 회원 여부 판단 로직 추가

---
+++ app/src/main/java/com/example/rundraw_fe/auth/MemberResponse.java
@@ -0,0 +1,12 @@
+package com.example.rundraw_fe.auth;
+
+public class MemberResponse {
+    private Long id;
+    private String name;
+    public Long getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+}
