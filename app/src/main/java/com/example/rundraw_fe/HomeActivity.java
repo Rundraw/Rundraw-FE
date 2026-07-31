9cb6efd12f5af5e9275024a36d8268d706d0e76c
정윤선 <yj2434632@gmail.com>
2026-07-31 17:58:40 +0900

    feat:홈 파일 수정

--- app/src/main/java/com/example/rundraw_fe/HomeActivity.java
+++ app/src/main/java/com/example/rundraw_fe/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.rundraw_fe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +14,10 @@ public class HomeActivity extends BaseActivity {
     private TextView tvRestaurantCourse;
     private TextView tvRestaurantRating;
 
+    private LinearLayout sectionMyRoute;
+    private LinearLayout sectionMyRestaurant;
+    private LinearLayout sectionGpsArt;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,7 @@ public class HomeActivity extends BaseActivity {
 
         initViews();
         loadSavedRestaurant();
+        setupSectionClickListeners();
         setupBottomNavigation(R.id.navigation_home);
     }
 
@@ -27,6 +34,26 @@ public class HomeActivity extends BaseActivity {
         tvRestaurantAddress = findViewById(R.id.tvRestaurantAddress);
         tvRestaurantCourse = findViewById(R.id.tvRestaurantCourse);
         tvRestaurantRating = findViewById(R.id.tvRestaurantRating);
+
+        sectionMyRoute = findViewById(R.id.sectionMyRoute);
+        sectionMyRestaurant = findViewById(R.id.sectionMyRestaurant);
+        sectionGpsArt = findViewById(R.id.sectionGpsArt);
+    }
+
+    private void setupSectionClickListeners() {
+        sectionMyRoute.setOnClickListener(v -> {
+            // TODO: 내 경로 목록 화면 아직 없음 - 만들어지면 연결
+            // startActivity(new Intent(HomeActivity.this, MyRouteListActivity.class));
+        });
+
+        sectionMyRestaurant.setOnClickListener(v -> {
+            // TODO: 저장한 맛집 목록 화면 아직 없음 - 만들어지면 연결
+            // startActivity(new Intent(HomeActivity.this, MyRestaurantListActivity.class));
+        });
+
+        sectionGpsArt.setOnClickListener(v -> {
+            startActivity(new Intent(HomeActivity.this, GpsArtActivity.class));
+        });
     }
 
     // TODO: 실제로는 서버 API 응답으로 채워야 함 (MypageService 쪽 API 붙일 자리)
